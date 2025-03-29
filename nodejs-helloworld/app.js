06b7658803a53da903cb9fb82003e2a1f3eca661
Rhythm Chaudhary <111283807+Rhythm1337@users.noreply.github.com>
2025-03-30 00:14:38 +0530

    Update app.js

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 app = express();
-
+// hi
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
@@ -8,4 +8,3 @@ app.get('/', function (req, res) {
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
-
